36311017a0584c2f1aa4c5ef4b9a2c22daef2362
Ali <qali372@gmail.com>
2024-03-21 13:30:35 +0200

    refactor : use OrderRepository in OrderController

--- src/main/java/com/seinfeld/tacocloud/controllers/OrderController.java
+++ src/main/java/com/seinfeld/tacocloud/controllers/OrderController.java
@@ -1,8 +1,10 @@
 package com.seinfeld.tacocloud.controllers;
 
 import com.seinfeld.tacocloud.models.TacoOrder;
+import com.seinfeld.tacocloud.repositories.OrderRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,13 @@ import org.springframework.web.bind.support.SessionStatus;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
@@ -29,7 +38,7 @@ public class OrderController {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepository.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
