c619d5dc8c7170ae29b546138221e46349dfe649
Ali <qali372@gmail.com>
2024-03-21 14:14:18 +0200

    refactor : IngredientRepository to use spring data jdbc

--- src/main/java/com/seinfeld/tacocloud/repositories/IngredientRepository.java
+++ src/main/java/com/seinfeld/tacocloud/repositories/IngredientRepository.java
@@ -1,14 +1,11 @@
 package com.seinfeld.tacocloud.repositories;
 
 import com.seinfeld.tacocloud.models.Ingredient;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
-    Iterable<Ingredient> findAll();
 
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
 }
