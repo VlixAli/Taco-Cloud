7b32b1a53e7685b265c3297a69b502e683f2074b
Ali <qali372@gmail.com>
2024-03-21 14:15:34 +0200

    feat : define Ingredient model as a table

--- src/main/java/com/seinfeld/tacocloud/models/Ingredient.java
+++ src/main/java/com/seinfeld/tacocloud/models/Ingredient.java
@@ -1,10 +1,18 @@
 package com.seinfeld.tacocloud.models;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table
+@AllArgsConstructor
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
