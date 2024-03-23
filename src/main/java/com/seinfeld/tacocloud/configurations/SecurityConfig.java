c0166273497b48fc55906fc007ba51dde5507a2a
Ali <qali372@gmail.com>
2024-03-24 01:47:52 +0200

    fix : replace deprecated @EnableGlobalMethodSecurity with @EnableMethodSecurity in SecurityConfig.java

--- src/main/java/com/seinfeld/tacocloud/configurations/SecurityConfig.java
+++ src/main/java/com/seinfeld/tacocloud/configurations/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableGlobalMethodSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
