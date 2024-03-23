39fc3d2d25c312727517a10d123c98a1e7503532
Ali <qali372@gmail.com>
2024-03-24 00:44:08 +0200

    build : RegistrationController ,  RegistrationForm.java and registration.html view

---
+++ src/main/java/com/seinfeld/tacocloud/controllers/RegistrationController.java
@@ -0,0 +1,30 @@
+package com.seinfeld.tacocloud.controllers;
+
+import com.seinfeld.tacocloud.models.RegistrationForm;
+import com.seinfeld.tacocloud.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    public String registerForm(){
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form){
+        userRepository.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
