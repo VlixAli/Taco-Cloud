6581ec94c603f7878075ea3ffeb5877f2f60f109
Ali <qali372@gmail.com>
2024-03-23 15:25:20 +0200

    refactor : remove jpa methods from OrderRepository to use mongodb

--- src/main/java/com/seinfeld/tacocloud/repositories/IngredientRepository.java
+++ src/main/java/com/seinfeld/tacocloud/repositories/IngredientRepository.java
@@ -3,8 +3,6 @@ package com.seinfeld.tacocloud.repositories;
 import com.seinfeld.tacocloud.models.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
 
--- src/main/java/com/seinfeld/tacocloud/repositories/OrderRepository.java
+++ src/main/java/com/seinfeld/tacocloud/repositories/OrderRepository.java
@@ -3,13 +3,6 @@ package com.seinfeld.tacocloud.repositories;
 import com.seinfeld.tacocloud.models.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Date;
-import java.util.List;
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
-
-    List<TacoOrder> findByDeliveryZip(String deliveryZip);
-
-    List<TacoOrder> readTacoOrderByDeliveryZipAndPlacedAtBetween(
-            String deliveryZip, Date startDate, Date endDate);
 }
